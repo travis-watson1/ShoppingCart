86817a337e24c3b412689c4eb3e2ae34cdd10bc4
Travis Watson <watsonjamest@gmail.com>
2020-04-08 09:47:36 -0500

    Db

---
+++ Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext : DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options) :base(options)
+        {
+
+        }
+
+
+    }
+}
