853ce16fa4e80485c5e8a6f3c0b3bcbfb8f72174
Travis Watson <watsonjamest@gmail.com>
2020-04-09 13:14:54 -0500

    Add create action to products controller with image preview

---
+++ Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+
+        private readonly CmsShoppingCartContext context;
+
+        public ProductsController(CmsShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await context.Products.OrderByDescending(x => x.Id).Include(x => x.Category).ToListAsync());
+        }
+
+        // GET /admin/products/create
+        public IActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name");
+
+            return View();
+        }
+
+    }
+}
--- Infrastructure/CmsShoppingCartContext.cs
+++ Infrastructure/CmsShoppingCartContext.cs
@@ -16,6 +16,8 @@ namespace CmsShoppingCart.Infrastructure
 
         public DbSet<Page> Pages { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
 
     }
 }
