70ca945cfa4c5f9692b3b3781f9fe7ea91862204
Travis Watson <watsonjamest@gmail.com>
2020-04-09 12:40:19 -0500

    Add delete and edit action to categoriescontroller. Add sorting to categoriescontroller

--- Areas/Admin/Controllers/CategoriesController.cs
+++ Areas/Admin/Controllers/CategoriesController.cs
@@ -57,5 +57,80 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             return View(category);
         }
 
+        // GET /admin/categories/edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            Category category = await context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST /admin/categories/edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.ToLower().Replace(" ", "-");
+
+                var slug = await context.Categories.Where(x => x.Id != id).FirstOrDefaultAsync(x => x.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The category already exists.");
+                    return View(category);
+                }
+
+                context.Update(category);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The category has been edited.";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET /admin/categories/delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            Category category = await context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                TempData["Error"] = "The category does not exist.";
+            }
+            else
+            {
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+                TempData["Success"] = "The category has been deleted.";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Reorder(int[] id)
+        {
+            int count = 1;
+
+            foreach (var categoryId in id)
+            {
+                Category category = await context.Categories.FindAsync(categoryId);
+                category.Sorting = count;
+                context.Update(category);
+                await context.SaveChangesAsync();
+                count++;
+            }
+
+            return Ok();
+        }
     }
 }
