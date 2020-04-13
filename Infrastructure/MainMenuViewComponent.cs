ad975d5b554cba707d0e4454b976818497f4279b
Travis Watson <watsonjamest@gmail.com>
2020-04-13 10:58:26 -0500

    Add products controller and index for customer view

---
+++ Infrastructure/MainMenuViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+        private readonly CmsShoppingCartContext context;
+
+        public MainMenuViewComponent(CmsShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pages = await GetPagesAsync();
+            return View(pages);
+        }
+
+        private Task<List<Page>> GetPagesAsync()
+        {
+            return context.Pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,16 @@ namespace CmsShoppingCart
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    "pages",
+                    "{slug?}",
+                    defaults: new {controller = "Pages", action = "Page" });
+
+                endpoints.MapControllerRoute(
+                    "products",
+                    "products/{categorySlug}",
+                    defaults: new { controller = "Products", action = "ProductsByCategory" });
+
+                endpoints.MapControllerRoute(
                     name: "areas",
                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
