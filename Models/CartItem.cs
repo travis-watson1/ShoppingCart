57cab547b6246b12849a07fb148fe24428222cc7
Travis Watson <watsonjamest@gmail.com>
2020-04-13 12:28:48 -0500

    Add add action to cart controller

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -14,6 +14,11 @@ namespace CmsShoppingCart.Models
         public decimal Total { get { return Quantity * Price; } }
         public string Image { get; set; }
 
+
+        public CartItem()
+        {
+                
+        }
         public CartItem(Product product)
         {
             ProductId = product.Id;
