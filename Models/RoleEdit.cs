25dc8e410daffe4464caffb1560228ddceca5f40
Travis Watson <watsonjamest@gmail.com>
2020-04-14 14:55:42 -0500

    Add roles controler to admin area

---
+++ Models/RoleEdit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CmsShoppingCart.Models
+{
+    public class RoleEdit
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<AppUser> Members { get; set; }
+        public IEnumerable<AppUser> NonMembers { get; set; }
+        public string RoleName { get; set; }
+        public string[] AddIds { get; set; }
+        public string[] DeleteIds { get; set; }
+    }
+}
