4bacda899239057a1022d724eeca0ce646b39cd6
Travis Watson <watsonjamest@gmail.com>
2020-04-14 10:59:31 -0500

    Add Identity model to project

---
+++ Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CmsShoppingCart.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string Occupation { get; set; }
+
+    }
+}
